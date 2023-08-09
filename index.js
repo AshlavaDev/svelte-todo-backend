6b498115ca3d503259de8b4bab6c515be5cf981a
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-08-08 22:13:03 -0600

    add cors

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const Todo = require('./models/Todo');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 mongoose.set('strictQuery', false);
